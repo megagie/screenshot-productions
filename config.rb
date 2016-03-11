2d247ede375dfdf199803b37db518921bed44fc6
megagie <mfarring@SMC-WIMDEPT-MBP.local>
2016-03-10 17:22:50 -0800

    fix relative link paths
    fix gh pages need for repo name in link path, appended repo name to
    build config for all link_to helpers

--- config.rb
+++ config.rb
@@ -36,6 +36,7 @@ end
 
 # Build-specific configuration
 configure :build do
+  set :http_prefix, '/screenshot-productions'
   # Minify CSS on build
   # activate :minify_css
 
@@ -52,3 +53,4 @@ activate :deploy do |deploy|
   # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
   #deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
 end
+
