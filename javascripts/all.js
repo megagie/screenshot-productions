040c2f9750e0699e1dab2f1c2cb8fa5a0ae17d65
megagie <megagie@users.noreply.github.com>
2016-03-11 02:41:15 -0800

    Automated commit at 2016-03-11 10:41:15 UTC by middleman-deploy 2.0.0-alpha

--- javascripts/all.js
+++ javascripts/all.js
@@ -1,1 +1,17 @@
-// This is where it all goes :)
+
+
+// Navigation
+
+$(document).ready(function() {
+  var menuToggle = $('#js-mobile-menu').unbind();
+  $('#js-navigation-menu').removeClass("show");
+
+  menuToggle.on('click', function(e) {
+    e.preventDefault();
+    $('#js-navigation-menu').slideToggle(function(){
+      if($('#js-navigation-menu').is(':hidden')) {
+        $('#js-navigation-menu').removeAttr('style');
+      }
+    });
+  });
+});
