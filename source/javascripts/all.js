e856462fa3d28fe558738f92cc5f11c45035a6f9
megagie <megagie@users.noreply.github.com>
2016-04-01 15:54:23 -0700

    refactor

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,39 +1,8 @@
 
+$(document).ready(function(){
+    $(".dropdown-button").dropdown();
+  });
 
-// Navigation
-
-$(document).ready(function() {
-  var menuToggle = $('#js-mobile-menu').unbind();
-  $('#js-navigation-menu').removeClass("show");
-
-  menuToggle.on('click', function(e) {
-    e.preventDefault();
-    $('#js-navigation-menu').slideToggle(function(){
-      if($('#js-navigation-menu').is(':hidden')) {
-        $('#js-navigation-menu').removeAttr('style');
-      }
+$(document).ready(function(){
+      $('.parallax').parallax();
     });
-  });
-});
-
-
-// Modal
-
-$(function() {
-  $("#modal-1").on("change", function() {
-    if ($(this).is(":checked")) {
-      $("body").addClass("modal-open");
-    } else {
-      $("body").removeClass("modal-open");
-    }
-  });
-
-  $(".modal-fade-screen, .modal-close").on("click", function() {
-    $(".modal-state:checked").prop("checked", false).change();
-  });
-
-  $(".modal-inner").on("click", function(e) {
-    e.stopPropagation();
-  });
-});
-
