978f2ad6c7e5b850c6d13805f592b8a480bd9543
megagie <megagie@users.noreply.github.com>
2016-04-05 18:28:25 -0700

    membership

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -5,8 +5,8 @@ $(document).ready(function(){
       outDuration: 225,
       constrain_width: false, // Does not change width of dropdown to that of the activator
       hover: false, // Activate on hover
-      gutter: 10, // Spacing from edge
-      belowOrigin: true, // Displays dropdown below the button
+      gutter: 0, // Spacing from edge
+      belowOrigin: false, // Displays dropdown below the button
       alignment: 'left' // Displays dropdown with edge aligned to the left of button
     });
   });
