2e4a4623232714de207c02c695f46d3d84bf0260
Meghan Farrington <megagie@users.noreply.github.com>
2016-04-08 11:33:21 -0700

    Automated commit at 2016-04-08 18:33:20 UTC by middleman-deploy 2.0.0-alpha

---
+++ javascripts/all.js
@@ -0,0 +1,21 @@
+
+$(document).ready(function(){
+    $(".dropdown-button").dropdown({
+      inDuration: 300,
+      outDuration: 225,
+      constrain_width: false, // Does not change width of dropdown to that of the activator
+      hover: false, // Activate on hover
+      gutter: 0, // Spacing from edge
+      belowOrigin: false, // Displays dropdown below the button
+      alignment: 'left' // Displays dropdown with edge aligned to the left of button
+    });
+  });
+
+$(document).ready(function(){
+      $('.parallax').parallax();
+    });
+
+$(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal();
+  });
